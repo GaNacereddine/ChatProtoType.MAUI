e669e4e2ec5796b779fa628359fa9943cfd27b12
Galfout Nasreddine <GNasreddine@outlook.com>
2022-09-27 00:52:50 +0100

    IOS support

--- ChatProtoType.MAUI/Controls/Handlers/PointerInputViewHandler.cs
+++ ChatProtoType.MAUI/Controls/Handlers/PointerInputViewHandler.cs
@@ -1,6 +1,9 @@
 ﻿#nullable enable
 #if __IOS__ || MACCATALYST
 using MauiPlatformView = Microsoft.Maui.Platform.ContentView;
+using UIKit;
+using Foundation;
+using Microsoft.Maui.Graphics.Platform;
 #elif __ANDROID__
 using MauiPlatformView = Microsoft.Maui.Platform.ContentViewGroup;
 using Android.Views;
@@ -106,16 +109,82 @@ namespace ChatProtoType.MAUI.Handlers
 #endif
 
 #if IOS || MACCATALYST
+
+    public class InputContentView : MauiPlatformView, IMAUIPointerInputView
+    {
+        public event PointerEventHandler? PointerChanged;
+
+        public override void TouchesBegan(NSSet touches, UIEvent? evt)
+        {
+            base.TouchesBegan(touches, evt);
+
+            var touch = (UITouch)touches.AnyObject;
+            OnPress(touch.PreviousLocationInView(this).AsPointF());
+        }
+
+        /// <inheritdoc />
+        public override void TouchesMoved(NSSet touches, UIEvent? evt)
+        {
+            base.TouchesMoved(touches, evt);
+            var touch = (UITouch)touches.AnyObject;
+            var currentPoint = touch.LocationInView(this);
+            OnMoving(currentPoint.AsPointF());
+        }
+
+        /// <inheritdoc />
+        public override void TouchesEnded(NSSet touches, UIEvent? evt)
+        {
+            base.TouchesEnded(touches, evt);
+            var touch = (UITouch)touches.AnyObject;
+            var currentPoint = touch.LocationInView(this);
+            OnRelease(currentPoint.AsPointF());
+        }
+
+        /// <inheritdoc />
+        public override void TouchesCancelled(NSSet touches, UIEvent? evt)
+        {
+            base.TouchesCancelled(touches, evt);
+            var touch = (UITouch)touches.AnyObject;
+            var currentPoint = touch.LocationInView(this);
+            OnRelease(currentPoint.AsPointF());
+        }
+
+        private void OnRelease(PointF point)
+        {
+            PointerChanged?.Invoke(this, new(point.X, point.Y, PointerState.Up));
+        }
+
+        private void OnMoving(PointF point)
+        {
+            PointerChanged?.Invoke(this, new(point.X, point.Y, PointerState.Move));
+        }
+
+        private void OnPress(PointF point)
+        {
+            PointerChanged?.Invoke(this, new(point.X, point.Y, PointerState.Down));
+        }
+    }
+
     public partial class PointerInputViewHandler 
     {
+        protected override MauiPlatformView CreatePlatformView()
+        {
+            return new InputContentView();
+        }
+
         protected void HookInputEvents(MauiPlatformView platformView)
         {
-            //platformView. += PlatformView_Touch;
+            ((InputContentView)platformView).PointerChanged += PointerInputViewHandler_PointerChanged;
+        }
+
+        private void PointerInputViewHandler_PointerChanged(object sender, PointerEventArgs e)
+        {
+            PointerInputVirtualView.RaisePointerChanged(e);
         }
 
         protected void UnHookInputEvents(MauiPlatformView platformView)
         {
-            //platformView.Touch -= PlatformView_Touch;
+            ((InputContentView)platformView).PointerChanged -= PointerInputViewHandler_PointerChanged;
         }
 
     }
